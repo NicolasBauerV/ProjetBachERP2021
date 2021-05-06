266a4c6ebc762de934e89a06a4630ad357cffb00
NicolasBauerV <bauer.nicolas266@gmail.com>
2021-05-06 09:25:20 +0200

    add index.php

---
+++ pages/index.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ludus-ERP : Index</title>
+    <link type="text/css" rel="stylesheet" href="../src/style/index.css?t=<? echo time(); ?>"/>
+</head>
+<body>
+    <header>
+        <h2 style="margin-left: .4em;">Bienvenue, séléctionner la page à charger...</h2>
+    </header>
+
+    <div id="wrapper">
+        <div id="items">
+            <ul>
+                <button onclick="location.href='./accueil.php'">Accueil administration</button>
+                <button onclick="location.href='./formulaire_renseignement.php'">Formulaire renseignement</button>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
